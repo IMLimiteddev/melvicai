885c71e63933f0723db6bb04bfd40d0deb35ce49
clintonace <clintonace09@gmail.com>
2026-03-30 11:35:56 +0100

    dashboard setup

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -12,5 +12,33 @@
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
+<!-- dashboard stylings from templates -->
+
+<link rel="stylesheet" type="text/css" href="/dash/assets/css/font-awesome.css">
+ <!-- ico-font-->
+<link rel="stylesheet" type="text/css" href="/dash/assets/css/vendors/icofont.css">
+<!-- Themify icon-->
+<link rel="stylesheet" type="text/css" href="/dash/assets/css/vendors/themify.css">
+<!-- Flag icon-->
+<link rel="stylesheet" type="text/css" href="/dash/assets/css/vendors/flag-icon.css">
+<!-- Feather icon-->
+<link rel="stylesheet" type="text/css" href="/dash/assets/css/vendors/feather-icon.css">
+<!-- Plugins css start-->
+<link rel="stylesheet" type="text/css" href="/dash/assets/css/vendors/slick.css">
+<link rel="stylesheet" type="text/css" href="/dash/assets/css/vendors/slick-theme.css">
+<link rel="stylesheet" type="text/css" href="/dash/assets/css/vendors/scrollbar.css">
+<link rel="stylesheet" type="text/css" href="/dash/assets/css/vendors/animate.css">
+<link rel="stylesheet" type="text/css" href="/dash/assets/css/vendors/datatables.css">
+<link rel="stylesheet" type="text/css" href="/dash/assets/css/vendors/owlcarousel.css">
+<!-- Plugins css Ends-->
+<!-- Bootstrap css-->
+<link rel="stylesheet" type="text/css" href="/dash/assets/css/vendors/bootstrap.css">
+<!-- App css-->
+<link rel="stylesheet" type="text/css" href="/dash/assets/css/style.css">
+<link id="color" rel="stylesheet" href="/dash/assets/css/color-1.css" media="screen">
+<!-- Responsive css-->
+<link rel="stylesheet" type="text/css" href="/dash/assets/css/responsive.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+<!-- template laravel stylings -->
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxAppearance
