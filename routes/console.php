2cbbb996a0ffd472499f484851617fa0734cbfef
clintonace <clintonace09@gmail.com>
2026-09-11 11:05:48 +0100

    c

--- routes/console.php
+++ routes/console.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
+
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
@@ -13,3 +14,10 @@ Artisan::command('inspire', function () {
 Schedule::command('workflows:process')
     ->everyMinute()
     ->withoutOverlapping();
+
+
+Schedule::command(
+    'queue:work --stop-when-empty --tries=1 --timeout=900'
+)
+->everyMinute()
+->withoutOverlapping();
